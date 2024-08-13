7f30879a1968ab44884031b851d0d6b2fcfed8de
Durga <durgamurali2011@gmail.com>
2024-08-12 18:33:45 -0700

    Time: 3 ms (51.67%), Space: 13.4 MB (31.66%) - LeetHub

---
+++ 0103-binary-tree-zigzag-level-order-traversal/0103-binary-tree-zigzag-level-order-traversal.cpp
@@ -0,0 +1,74 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    vector<vector<int>> zigzagLevelOrder(TreeNode* root) {
+        vector<vector<int>> result;
+        if(root == NULL)
+            return result;
+
+        std::queue<TreeNode*> odd_queue;
+        std::queue<TreeNode*> even_queue;
+        odd_queue.push(root);
+        int level_count = 1;
+        vector<int> level_nodes;
+
+        while(!odd_queue.empty() || !even_queue.empty())
+        {
+            if(level_count % 2 != 0)
+            {
+                TreeNode* node = odd_queue.front();
+                odd_queue.pop();
+
+                if(node != NULL)
+                {
+                    level_nodes.push_back(node->val);
+                    if(node->left != NULL)
+                        even_queue.push(node->left);
+                    if(node->right != NULL)
+                        even_queue.push(node->right);
+                }
+
+                if(odd_queue.empty())
+                {
+                    level_count++;
+                    result.push_back(level_nodes);
+                    level_nodes.clear();
+                }
+            }
+            else if(level_count % 2 == 0)
+            {
+                TreeNode* node = even_queue.front();
+                even_queue.pop();
+
+                if(node != NULL)
+                {
+                    level_nodes.push_back(node->val);
+                    if(node->left != NULL)
+                        odd_queue.push(node->left);
+                    if(node->right != NULL)
+                        odd_queue.push(node->right);
+                }
+
+                if(even_queue.empty())
+                {
+                    level_count++;
+                    vector<int> reverse_level_nodes(level_nodes.rbegin(), level_nodes.rend());
+                    result.push_back(reverse_level_nodes);
+                    level_nodes.clear();
+                }
+            }
+        }
+        
+        return result;
+    }
+};
